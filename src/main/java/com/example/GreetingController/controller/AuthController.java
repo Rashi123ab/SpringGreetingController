49f90cf789cd6ed8c9884ab45cdb4df5f348935b
Rashi123ab <rashia729@gmail.com>
2025-03-05 10:27:21 +0530

    Adding SMTP to send mail notification on login

--- src/main/java/com/example/GreetingController/controller/AuthController.java
+++ src/main/java/com/example/GreetingController/controller/AuthController.java
@@ -1,20 +1,23 @@
 package com.example.GreetingController.controller;
 import com.example.GreetingController.DTOs.AuthUserDTO;
 import com.example.GreetingController.DTOs.LoginDTO;
+import com.example.GreetingController.repository.AuthUserRepository;
 import com.example.GreetingController.service.AuthService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/auth")
+@CrossOrigin(origins = "*")
 @RequiredArgsConstructor
 public class AuthController {
 
     private final AuthService authService;
-
-
+    private final AuthUserRepository authUserRepository;
+    private final BCryptPasswordEncoder passwordEncoder;
 
 
     @PostMapping("/register")
@@ -25,7 +28,9 @@ public class AuthController {
 
     }
     @PostMapping("/login")
-    public ResponseEntity<String> loginUser(@Valid @RequestBody LoginDTO loginDTO) {
-        return ResponseEntity.ok(authService.loginUser(loginDTO));
+    public ResponseEntity<String> loginUser(@RequestBody LoginDTO loginDTO) {
+        String response = authService.loginUser(loginDTO);
+        return ResponseEntity.ok(response);
     }
+
 }
